884c3606cc3a32f6d22efef285f5c013ca64e267
David <davidostos2@gmail.com>
2023-03-08 16:36:48 -0400

    agregando imagenes a imgur

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,20 +7,20 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'sanFrancisco': "url('../img/sanFrancisco.jpg')",
-        'sanFranciscoDesktop': "url('../img/sanFranciscoDesktop.jpg')",
-        'yosemite': "url('../img/yosemite.jpg')",
-        'LA': "url('../img/LA.jpg')",
-        'seattle': "url('../img/seattle.jpg')",
-        'new_york': "url('../img/new_york.jpg')",
-        'norway': "url('../img/norway.jpg')",
-        'sydney': "url('../img/sydney.jpg')",
-        'miami': "url('../img/miami.jpg')",
-        'switzerland': "url('../img/switzerland.jpg')",
-        'bali': "url('../img/bali.jpg')",
-        'chicago': "url('../img/chicago.jpg')",
-        'europe': "url('../img/europe.jpg')",
-        'iceland': "url('../img/iceland.jpg')"
+        'sanFrancisco': "url('https://i.imgur.com/b6oe9Zw.jpg')",
+        'sanFranciscoDesktop': "url('https://i.imgur.com/aPUiS8O.jpg')",
+        'yosemite': "url('https://i.imgur.com/yFqQOzM.jpg')",
+        'LA': "url('https://i.imgur.com/TDx4a42.jpg')",
+        'seattle': "url('https://i.imgur.com/1xbdsUk.jpg')",
+        'new_york': "url('https://i.imgur.com/afl0FWi.jpg')",
+        'norway': "url('https://i.imgur.com/9uKcXKD.jpg')",
+        'sydney': "url('https://i.imgur.com/PDzEgCZ.jpg')",
+        'miami': "url('https://i.imgur.com/vGIzXcL.jpg')",
+        'switzerland': "url('https://i.imgur.com/jsjrR7g.jpg')",
+        'bali': "url('https://i.imgur.com/3Atkgl6.jpg')",
+        'chicago': "url('https://i.imgur.com/LiU0BLh.jpg')",
+        'europe': "url('https://i.imgur.com/S8PYmhb.jpg')",
+        'iceland': "url('https://i.imgur.com/EpOf7dU.jpg')"
       },
       backgroundColor: theme =>({
         ...theme('colors'),
