81349e2abe7daf21a2a779135a620c65b12f485f
David <davidostos2@gmail.com>
2023-03-09 15:56:41 -0400

    clase 5 agregando nab-bar y comenzando a crear el responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,20 +7,20 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'sanFrancisco': "url('https://i.imgur.com/b6oe9Zw.jpg')",
-        'sanFranciscoDesktop': "url('https://i.imgur.com/aPUiS8O.jpg')",
-        'yosemite': "url('https://i.imgur.com/yFqQOzM.jpg')",
-        'LA': "url('https://i.imgur.com/TDx4a42.jpg')",
-        'seattle': "url('https://i.imgur.com/1xbdsUk.jpg')",
-        'new_york': "url('https://i.imgur.com/afl0FWi.jpg')",
-        'norway': "url('https://i.imgur.com/9uKcXKD.jpg')",
-        'sydney': "url('https://i.imgur.com/PDzEgCZ.jpg')",
-        'miami': "url('https://i.imgur.com/vGIzXcL.jpg')",
-        'switzerland': "url('https://i.imgur.com/jsjrR7g.jpg')",
-        'bali': "url('https://i.imgur.com/3Atkgl6.jpg')",
-        'chicago': "url('https://i.imgur.com/LiU0BLh.jpg')",
-        'europe': "url('https://i.imgur.com/S8PYmhb.jpg')",
-        'iceland': "url('https://i.imgur.com/EpOf7dU.jpg')"
+        'sanFrancisco': "url('https://i.imgur.com/8rdEA7W.jpg')",
+        'sanFranciscoDesktop': "url('https://i.imgur.com/6QBRerV.jpg')",
+        'yosemite': "url('https://i.imgur.com/TelXD63.jpg')",
+        'LA': "url('https://i.imgur.com/X1WT8kG.jpg')",
+        'seattle': "url('https://i.imgur.com/A668ntf.jpg')",
+        'new_york': "url('https://i.imgur.com/J3Cd2qh.jpg')",
+        'norway': "url('https://i.imgur.com/nKJkNrl.jpg' )",
+        'sydney': "url('https://i.imgur.com/7uJWIA1.jpg' )",
+        'miami': "url('https://i.imgur.com/WhkQuaq.jpg')",
+        'switzerland': "url('https://i.imgur.com/oH6teiS.jpg')",
+        'bali': "url('https://i.imgur.com/21rIhog.jpg')",
+        'chicago': "url('https://i.imgur.com/M1HhCtr.jpg')",
+        'europe': "url('https://i.imgur.com/O2hs2A0.jpg')",
+        'iceland': "url('https://i.imgur.com/ilYjUzn.jpg')"
       },
       backgroundColor: theme =>({
         ...theme('colors'),
